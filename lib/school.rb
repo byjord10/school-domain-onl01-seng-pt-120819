4fb67cad008293d5fac972b6501db16e896a6fa6
Brianna Jordan <bryvjordan9@gmail.com>
2020-01-11 05:58:19 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+class School
+  attr_accessor :roster, :name
+
+   def initialize(roster, name)
+     @name = name
+     @roster = {}
+     ROSTER << roster
+   end
+
+   def add_student(student_name, grade)
+    roster[grade] ||= []
+    roster[grade] << student_name
+  end
+
+
+
+
+end
